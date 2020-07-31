70daab819f420e9a4635add1bf6db0bd291d856b
reborn233 <499289113@qq.com>
2020-07-31 17:14:03 +0800

    新增方法

--- js/simpleQuery.js
+++ js/simpleQuery.js
@@ -33,6 +33,90 @@ const createCollection = collection => {
     }
   }
 
+  collection.show = () => {
+    collection.forEach(element => {
+      element.style.display = '';
+    })
+  }
+
+  collection.hide = () => {
+    collection.forEach(element => {
+      element.style.display = 'none'
+    })
+  }
+
+  collection.toggle = () => {
+    collection.forEach(element => {
+      if (element.ownerDocument.defaultView.getComputedStyle(element, null).display === 'none') {
+        element.style.display = '';
+      } else {
+        element.style.display = 'none';
+      }
+    })
+  }
+
+  collection.fadeIn = (ms = 300) => {
+    collection.forEach(element => {
+      element.style.transition = `opacity ${ms / 1000}s`;
+      element.style.opacity = '1';
+    })
+  }
+  collection.fadeOut = (ms = 300) => {
+    collection.forEach(element => {
+      element.style.transition = `opacity ${ms / 1000}s`;
+      element.style.opacity = '0';
+    })
+  }
+
+  collection.fadeToggle = (ms = 300) => {
+    collection.forEach(el => {
+      el.style.transition = `opacity ${ms / 1000}s`;
+      const { opacity } = el.ownerDocument.defaultView.getComputedStyle(el, null);
+      if (opacity === '1') {
+        el.style.opacity = '0';
+      } else {
+        el.style.opacity = '1';
+      }
+    })
+  }
+
+  collection.slideUp = (ms = 300) => {
+    collection.forEach(element => {
+      const originHeight = element.getBoundingClientRect().height;
+      element.style.transition = `height ${ms / 1000}s`;
+      element.style.height = '0px';
+    })
+  }
+  collection.slideDown = (ms = 300, originHeight = 50) => {
+    collection.forEach(element => {
+      element.style.transition = `height ${ms / 1000}s`;
+      element.style.height = `${originHeight}px`;
+    })
+  }
+
+  collection.slideToggle = (ms = 300, originHeight = 50) => {
+    collection.forEach(element => {
+      element.style.transition = `height ${ms / 1000}s`;
+      const { height } = element.ownerDocument.defaultView.getComputedStyle(element, null);
+      if (parseInt(height, 10) === 0) {
+        element.style.height = `${originHeight}px`;
+      }
+      else {
+        element.style.height = '0px';
+      }
+    })
+  }
+
+  collection.animate = (params = {}, speed = 300) => {
+    const action = Object.keys(params);
+    collection.forEach(el => {
+      el.style.transition = `all ${speed / 1000}s`;
+      action.forEach(key => {
+        el.style[key] = params[key]
+      })
+    })
+  }
+
   collection.attr = (...args) => {
     if (args.length === 1) {
       const el = collection[0];
@@ -187,6 +271,12 @@ const createCollection = collection => {
     return collection[0].cloneNode()
   }
 
+  collection.empty = () => {
+    collection.forEach(element => {
+      element.innerHTML = '';
+    })
+  }
+
   collection.each = (callback) => {
     collection.forEach((element, index) => {
       const bindFunc = callback.bind(element);
@@ -206,6 +296,20 @@ const createCollection = collection => {
     })
   }
 
+  collection.load = async (url, callback) => {
+    try {
+      const response = await fetch(url)
+      const data = await response.text()
+      collection.forEach(element => {
+        element.innerHTML = data;
+      })
+      callback && callback(data)
+    }
+    catch (error) {
+
+    }
+  }
+
   return collection;
 }
 
@@ -222,3 +326,107 @@ const $ = (...args) => {
     return createCollection(collection);
   }
 };
+
+$.isArray = (array) => {
+  return Array.isArray(array);
+}
+$.isWindow = obj => {
+  return obj !== null && obj !== undefined && obj === obj.window;
+}
+$.inArray = (item, array) => {
+  return array.includes(item);;
+}
+$.isNumeric = n => {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+$.isFunction = item => {
+  if (typeof item === 'function') {
+    return true;
+  }
+  const type = Object.prototype.toString(item);
+  return type === '[object Function]' || type === '[object GeneratorFunction]';
+}
+$.isEmptyObject = obj => {
+  return Object.keys(obj).length === 0;
+}
+
+$.isPlainObject = obj => {
+  if (typeof (obj) !== 'object' || obj.nodeType || obj !== null && obj !== undefined && obj === obj.window) {
+    return false;
+  }
+
+  if (obj.constructor &&
+    !Object.prototype.hasOwnProperty.call(obj.constructor.prototype, 'isPrototypeOf')) {
+    return false;
+  }
+
+  return true;
+}
+
+$.extend = (...args) => {
+  return Object.assign(...args);
+}
+$.trim = (str) => {
+  return str.trim();
+}
+$.type = obj => {
+  const reTypeOf = /(?:^\[object\s(.*?)\]$)/;
+  return Object.prototype.toString.call(obj)
+    .replace(reTypeOf, '$1')
+    .toLowerCase();
+}
+$.merge = (...args) => {
+  return Array.from(new Set([].concat(...args)))
+}
+$.makeArray = arrayLike => {
+  return Array.from(arrayLike);
+}
+$.parseHTML = htmlString => {
+  const context = document.implementation.createHTMLDocument();
+  const base = context.createElement('base');
+  base.href = document.location.href;
+  context.head.appendChild(base);
+
+  context.body.innerHTML = htmlString;
+  return context.body.children;
+}
+
+$.getJson = (url, success, fail) => {
+  const request = new XMLHttpRequest()
+  request.open('GET', url, true)
+  request.onload = function () {
+    if (this.status >= 200 && this.status < 400) {
+      const data = JSON.parse(this.response)
+      success && success(data)
+    }
+    else {
+      fail && fail(this)
+    }
+  }
+
+  request.onerror = function (error) {
+    fail && fail(error)
+  };
+
+  request.send()
+}
+
+$.post = (url, data, success, fail) => {
+  const request = new XMLHttpRequest();
+  request.open('POST', url, true);
+  request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+  request.onload = function () {
+    if (this.status >= 200 && this.status < 400) {
+      const data = JSON.parse(this.response)
+      success && success(data)
+    }
+    else {
+      fail && fail(this)
+    }
+  }
+
+  request.onerror = function (error) {
+    fail && fail(error)
+  };
+  request.send(data);
+}
